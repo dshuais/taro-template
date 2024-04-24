33eb4cf9e2c94bdf7a7c013eb251e4fe77ce6d7a
dshuais <1137896420@qq.com>
2024-04-24 16:43:35 +0800

    ✨ feat: 配置tabbar

--- src/app.config.ts
+++ src/app.config.ts
@@ -2,7 +2,7 @@
  * @Author: dushuai
  * @Date: 2024-04-23 18:33:22
  * @LastEditors: dushuai
- * @LastEditTime: 2024-04-24 16:39:36
+ * @LastEditTime: 2024-04-24 16:42:40
  * @description: defineAppConfig
  */
 export default defineAppConfig({
@@ -49,5 +49,26 @@ export default defineAppConfig({
     navigationBarBackgroundColor: '#000',
     navigationBarTitleText: 'Taro Demo',
     navigationBarTextStyle: 'white'
+  },
+
+  // tabbar
+  tabBar: {
+    custom: false, // 是否自定义tabbar
+    color: '#fff',
+    selectedColor: '#885525',
+    backgroundColor: '#000',
+    borderStyle: 'black',
+    list: [
+      {
+        pagePath: 'pages/index/index',
+        // iconPath: 'static/images/tabbar/home.png',
+        // selectedIconPath: 'static/images/tabbar/home_active.png',
+        text: '首页',
+      },
+      {
+        pagePath: 'pages/my/index',
+        text: '我的',
+      }
+    ]
   }
 })
